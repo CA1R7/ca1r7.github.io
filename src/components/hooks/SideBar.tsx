1e360b6129d9419fc5573765cc7003973600b31f
CA1R7 <cairzz222@gmail.com>
2021-10-22 12:31:52 +0100

    Added `view source` button on side bar

--- src/components/hooks/SideBar.tsx
+++ src/components/hooks/SideBar.tsx
@@ -10,12 +10,16 @@ const SideBarComponent = styled.div<SideBarProps>`
 `;
 
 export const SideBar: FC<SideBarProps> = ({ active }) => {
-  let temp_text_style: CSSProperties = { display: "block", margin: "10px", fontSize: "15px" };
   return (
     <SideBarComponent id="side-bar" active={active}>
       <div id="logo">cairbyte71</div>
       <div id="routes-buttons">
-        <span style={temp_text_style}>Comming soon ...</span>
+        <div
+          className="button"
+          onClick={() => window.open("https://github.com/CA1R7/ca1r7.github.io")}
+        >
+          view source
+        </div>
       </div>
     </SideBarComponent>
   );
