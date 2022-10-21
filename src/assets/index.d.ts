cf9c4d423a1793f621c2fe00598e8035482861af
unknown <cairzz222@gmail.com>
2022-10-21 21:53:16 +0100

    Updates

--- src/assets/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "*.svg";
-declare module "*.svgn";
-declare module "*.jpg";
-declare module "*.png";
-declare module "*.gif";
-declare module "*.mp3";
-declare module "*.wav";
-declare module "*.webp";
--- src/utils/loadImage.ts
+++
@@ -1,8 +0,0 @@
-export const loadImage = (url: string) => {
-  return new Promise<HTMLImageElement>((resolve, reject) => {
-    const image = new Image();
-    image.onload = () => resolve(image);
-    image.onerror = reject;
-    image.src = url;
-  });
-};
